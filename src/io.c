a37bffb883162d37d4ee78784eeabb883d6cf0c1
dachr8 <32724398+dachr8@users.noreply.github.com>
2018-05-21 09:24:03 +0800

    #rename and simplify the IO function

--- src/io.c
+++ src/io.c
@@ -3,7 +3,7 @@
 #include <malloc.h>
 #include "struct.h"
 
-JOBPTR *getJobByKeyboard() {
+JOBPTR *getJobByStdin() {
     scanf("%d %d", &jobNum, &machineNum);
 
     JOBPTR job[jobNum] = {NULL};
@@ -18,7 +18,7 @@ JOBPTR *getJobByKeyboard() {
     return *(&job);
 }
 
-void getOverhaulByKeyboard() {
+void getOverhaulByStdin() {
     int num = 0;
     OVERHAULPTR tmp = overhaul = malloc(sizeof(struct overhaul));
     while (num != -1) {
@@ -28,33 +28,7 @@ void getOverhaulByKeyboard() {
     }
 }
 
-JOBPTR *getByFile() {
-    FILE *fp = fopen("input.txt", "r");
-    fscanf(fp, "%d %d", &jobNum, &machineNum);
-
-    JOBPTR job[jobNum] = {NULL};
-    int num = 0;
-    while (fscanf(fp, "%d", &num) && (num != -1)) {
-        JOBPTR tmp = job[num] = malloc(sizeof(struct job));
-        while (fscanf(fp, "(%d，%d)", &tmp->time, &tmp->machine) == 2)
-            tmp = tmp->nextMachine = malloc(sizeof(struct machine));
-        tmp->nextMachine = NULL;
-    }
-
-    num = 0;
-    OVERHAULPTR tmp = overhaul = malloc(sizeof(struct overhaul));
-    while (num != -1) {
-        while (fscanf(fp, "%d%d%d", &tmp->timeline, &overhaul->machine, &overhaul->time) == 3)
-            tmp = tmp->nextOverhaul = malloc(sizeof(struct overhaul));
-        fscanf(fp, "%d", &num);
-    }
-
-    fclose(fp);
-
-    return *(&job);
-}
-
-void outputOnScreen(MACHINEPTR *machine, JOBPTR *job) {
+void outputByStdout(MACHINEPTR *machine, JOBPTR *job) {
     for (int i = 0; i < machineNum; ++i) {
         printf("\nM%d", i);
         MACHINEPTR tmp = machine[i];
@@ -78,36 +52,6 @@ void outputOnScreen(MACHINEPTR *machine, JOBPTR *job) {
         }
     }
     printf("\nEnd %d\n", makeSpan);
-}
-
-void outputByFile(MACHINEPTR *machine, JOBPTR *job) {
-    FILE *fp = fopen("output.txt", "a");
-
-    for (int i = 0; i < machineNum; ++i) {
-        fprintf(fp, "\nM%d", i);
-        MACHINEPTR tmp = machine[i];
-        while (tmp != NULL) {
-            fprintf(fp, " (%d,", tmp->timeline);
-            if (tmp->job == -1)
-                fprintf(fp, "\"Overhaul\"");
-            else {
-                fprintf(fp, "%d-", tmp->job);
-                JOBPTR tmp2 = job[i];
-                for (int j = 0;; ++j) {
-                    if (tmp2->machine == i) {
-                        printf("%d", i);
-                        break;
-                    }
-                    tmp2 = tmp2->nextMachine;
-                }
-            }
-            fprintf(fp, ",%d)", tmp->time);
-            tmp = tmp->nextJob;
-        }
-    }
-    fprintf(fp, "\nEnd %d\n", makeSpan);
-
-    fclose(fp);
 }
 
 extern void freeAll(JOBPTR *job, MACHINEPTR *machine, OVERHAULPTR overhaul) {
--- src/main.c
+++ src/main.c
@@ -2,19 +2,15 @@
 #include <stdio.h>
 #include "struct.h"
 
-extern JOBPTR *getJobByKeyboard();
+extern JOBPTR *getJobByStdin();
 
-extern void getOverhaulByKeyboard();
-
-extern JOBPTR *getByFile();
+extern void getOverhaulByStdin();
 
 extern MACHINEPTR *schedule(JOBPTR *job);
 
 extern MACHINEPTR *reSchedule(MACHINEPTR *machine, JOBPTR *job);
 
-extern void outputOnScreen(MACHINEPTR *machine, JOBPTR *job);
-
-extern void outputByFile(MACHINEPTR *machine, JOBPTR *job);
+extern void outputByStdout(MACHINEPTR *machine, JOBPTR *job);
 
 extern void freeAll(JOBPTR *job, MACHINEPTR *machine, OVERHAULPTR overhaul);
 
@@ -23,35 +19,36 @@ int main(void) {
     MACHINEPTR *machine = NULL;
 
     int inputMode;
-    printf("Input Mode: 1.Keyboard 2.File\n");
+    printf("IO Mode: 1.Keyboard 2.File\n");
     printf("Please input your input mode: ");
     scanf("%d", &inputMode);
     switch (inputMode) {
         case 1:
-            job = getJobByKeyboard();
+            job = getJobByStdin();
             printf("\nPlease wait for the output result...\n");
             machine = schedule(job);
-            outputOnScreen(machine, job);
-            outputByFile(machine, job);
+            outputByStdout(machine, job);
 
             printf("\nPlease enter overhaul information:\n");
-            getOverhaulByKeyboard();
+            getOverhaulByStdin();
             machine = reSchedule(machine, job);
-            outputOnScreen(machine, job);
-            outputByFile(machine, job);
+            outputByStdout(machine, job);
             break;
         case 2:
-            job = getByFile();
+            freopen("input.txt", "r", stdin);
+            job = getJobByStdin();
+            getOverhaulByStdin();
+            fclose(stdin);
             machine = schedule(job);
-            outputOnScreen(machine, job);
-            outputByFile(machine, job);
+            freopen("output.txt", "w", stdout);
+            outputByStdout(machine, job);
 
             machine = reSchedule(machine, job);
-            outputOnScreen(machine, job);
-            outputByFile(machine, job);
+            outputByStdout(machine, job);
+            fclose(stdout);
             break;
         default:
-            printf("Please input your input mode: ");
+            printf("Please input your IO mode: ");
             scanf("%d", &inputMode);
     }
     freeAll(job, machine, overhaul);
